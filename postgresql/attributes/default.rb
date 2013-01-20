4e52510f4a33594bad8e2fcd6bb0f25912a4f068
Damien Duportal <damien.duportal@gmail.com>
2013-01-20 10:25:08 +0100

    Adapting attributes for postgresql 8.4 by default, adding rpm repo url and postgresql needed conf to use with vbox NAT

--- postgresql/attributes/default.rb
+++ postgresql/attributes/default.rb
@@ -9,13 +9,13 @@
 # PostgreSQL Attributes
 node.set['postgresql'] = {
 	'version' => {
-		'major' => '9',
-		'minor' => '2',
-		'rpm' => '6'
+		'major' => '8',
+		'minor' => '4',
+		'repo_rpm' => 'http://yum.postgresql.org/8.4/redhat/rhel-6-x86_64/pgdg-centos-8.4-3.noarch.rpm'
 	},
 	'server' => {
 		'config' => {
-			'listen_addresses' => 'localhost',
+			'listen_addresses' => '*',
 			'max_connections' => 100,
 			'shared_buffers' => '32MB',
 			'log_destination' => 'stderr',
@@ -34,11 +34,10 @@ node.set['postgresql'] = {
 			'default_text_search_config' => 'pg_catalog.english',
 		},
 		'pg_hba' => [
-		  {:type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'ident'},
-		  {:type => 'local', :db => 'all', :user => 'all', :addr => nil, :method => 'ident'},
-		  {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5'},
-		  {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5'},
-		  {:type => 'host', :db => 'all', :user => 'all', :addr => '10.0.2.2/16', :method => 'md5' }
+		  {:type => 'local', :db => 'all', :user => 'all', :addr => nil, :method => 'trust'},
+		  {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'trust'},
+		  {:type => 'host', :db => 'all', :user => 'all', :addr => '10.0.2.2/16', :method => 'trust' },
+		  {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'trust'},
 		]
 	}
 }
