bd16d5f9e971a936921c3f09d49d326a656f5c65
Damien DUPORTAL <damien.duportal@gmail.com>
2013-04-23 18:20:30 +0200

    Adding attributes into serverinstall for vagrant

--- serverinstall/attributes/default.rb
+++ serverinstall/attributes/default.rb
@@ -9,3 +9,5 @@
 node.default['serverinstall']['packages'] = ["git","vim","curl","aptitude","ruby","rubygems","sysstat"]
 node.default['serverinstall']['profiles'] = ["alias-profile.sh","color-profile.sh"]
 node.set['virtualbox']['version'] = 4.2
+node.set['vagrant']['version'] = "1.0.7"
+node.set['vagrant']['versionUrl'] = "http://files.vagrantup.com/packages/1e2d92d0ed73e28346bb74345c8e353bcab415fb"
