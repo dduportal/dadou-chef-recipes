48d295f1c16b1906dbfd95e06687fd0a7f0b5cd7
Damien DUPORTAL <damien.duportal@gmail.com>
2013-04-23 10:42:13 +0200

    Some corrections on serverinstall cookbook

--- serverinstall/attributes/default.rb
+++ serverinstall/attributes/default.rb
@@ -8,3 +8,4 @@
 
 node.default['serverinstall']['packages'] = ["git","vim","curl","aptitude","ruby","rubygems","sysstat"]
 node.default['serverinstall']['profiles'] = ["alias-profile.sh","color-profile.sh"]
+node.set['virtualbox']['version'] = 4.2
