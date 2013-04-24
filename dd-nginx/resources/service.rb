484029bc84d3e4d7493076d837300cf4bd4bf47f
Damien DUPORTAL <damien.duportal@gmail.com>
2013-04-24 18:11:49 +0200

    corrections on dd-nginx service LWRP

--- dd-nginx/resources/service.rb
+++ dd-nginx/resources/service.rb
@@ -10,4 +10,4 @@ attribute :service_id, :kind_of => String, :name_attribute => true
 attribute :nginx_bin, :kind_of => String, :default => "/usr/sbin/nginx"
 attribute :service_user, :kind_of => String, :regex => Chef::Config[:user_valid_regex], :default => "www" 
 attribute :service_group, :kind_of => String, :regex => Chef::Config[:group_valid_regex], :default => "www"
-attribute :cookbook, :kind_of => String, :default => 'nginx' # Use this to overwrite cookbook's templates provider
+attribute :cookbook, :kind_of => String, :default => 'dd-nginx' # Use this to overwrite cookbook's templates provider
