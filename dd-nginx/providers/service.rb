c5f53f0d5292808f0c4fb79147a468ea6d3d6f7a
Damien DUPORTAL <damien.duportal@gmail.com>
2013-04-24 18:02:25 +0200

    Some modification into dd-nginx

--- dd-nginx/providers/service.rb
+++ dd-nginx/providers/service.rb
@@ -4,187 +4,79 @@
 action :create do
 	Chef::Log.info("Creating nginx service #{new_resource.service_id}")
 
-	service_id = new_resource.service_id
-	client_id = new_resource.client_id
-	serviceUser = new_resource.service_user
-	serviceGroup = new_resource.service_group
-	with_upstream = new_resource.with_upstream
-	is_atos = new_resource.is_atos
-	cookbook_for_tpl = new_resource.cookbook
-
-	serviceRootFolder = "#{new_resource.prefix_folder}/#{client_id}/#{service_id}"
-	confDir = "#{serviceRootFolder}/conf"
-	logsDir = "#{serviceRootFolder}/logs"
-	docrootDir = "#{serviceRootFolder}/docs"
-	tmpDir = "#{serviceRootFolder}/tmp"
-	if is_atos then
-  		nginx_bin = "/usr/#{node['nginx']['packagename']}-#{node['nginx']['version']}/sbin/nginx"
-  		nginx_pid_file = "#{logsDir}/nginx.pid"
-  	else
-  		nginx_bin = new_resource.nginx_bin
-  		nginx_pid_file = "#{tmpDir}/nginx.pid"
-  	end
+	service_root_folder = "#{new_resource.nginx_root_dir}/#{new_resource.service_id}"
+	conf_dir = "#{service_root_folder}/conf"
+	logs_dir = "#{service_root_folder}/logs"
+	docroot_dir = "#{service_root_folder}/docs"
+	tmp_dir = "#{service_root_folder}/tmp"
+	nginx_bin = new_resource.nginx_bin
+	nginx_pid_file = "#{tmp_dir}/nginx.pid"
 	
-	directory serviceRootFolder do
-		owner serviceUser
-		group serviceGroup
+	directory service_root_folder do
+		owner new_resource.service_user
+		group new_resource.service_group
 		mode 0755
 		action :create
 		recursive true
 	end
 
-	directory confDir do
-		owner serviceUser
-		group serviceGroup
+	directory conf_dir do
+		owner new_resource.service_user
+		group new_resource.service_group
 		mode 0755
 		action :create
 		recursive true
 	end
 
-	directory docrootDir do
-		owner serviceUser
-		group serviceGroup
+	directory docroot_dir do
+		owner new_resource.service_user
+		group new_resource.service_group
 		mode 0755
 		action :create
 		recursive true
 	end
 
-	directory logsDir do
-		owner serviceUser
-		group serviceGroup
+	directory logs_dir do
+		owner new_resource.service_user
+		group new_resource.service_group
 		mode 0755
 		action :create
 		recursive true
 	end
 
-	directory tmpDir do
-		owner serviceUser
-		group serviceGroup
+	directory tmp_dir do
+		owner new_resource.service_user
+		group new_resource.service_group
 		mode 0755
 		action :create
 		recursive true
 	end
 
-	directory "#{confDir}/vhosts" do
-		owner serviceUser
-		group serviceGroup
-		mode 0755
-		action :create
-		recursive true
-	end
-
-	directory "#{confDir}/locations" do
-		owner serviceUser
-		group serviceGroup
-		mode 0755
-		action :create
-		recursive true
-	end
-
-  	template "#{docrootDir}/index.html" do
+  	template "#{docroot_dir}/index.html" do
   		source "index.html.erb"
-  		cookbook cookbook_for_tpl
-		owner serviceUser
-		group serviceGroup
+  		cookbook new_resource.cookbook_for_tpl
+		owner new_resource.service_user
+		group new_resource.service_group
 		mode 0644
   		variables({
-	    	'service_name' => service_id,
+	    	'service_name' => new_resource.service_id,
   		})
   	end
-	template "#{confDir}/vhosts/#{service_id}.conf" do 
-  		source "default-vhost.conf.erb"
-  		cookbook cookbook_for_tpl
-  		owner serviceUser
-		group serviceGroup
-		mode 0644 
-		variables({
-    		'service_root_folder' => serviceRootFolder,
-    		'docroot' => docrootDir,
+
+  	## Add config here !
+
+	template "/etc/init.d/#{new_resource.service_id}-nginx" do
+  		source "init-script-nginx.erb"
+  		cookbook new_resource.cookbook_for_tpl
+		owner 'root'
+		group 'root'
+		mode 0755
+  		variables({
+    		'nginx_root_dir' => service_root_folder,
+    		'nginx_conf_dir' => conf_dir,
+    		'nginx_tmp_dir' => tmp_dir,
+    		'nginx_bin' => nginx_bin,
+    		'service_name' => service_id,
   		})
-  	end
-	cookbook_file "#{confDir}/mime.types" do
-  		source "mime.types"
-  		cookbook cookbook_for_tpl
-		owner serviceUser
-		group serviceGroup
-		mode 0644
-  	end
-
-  	if with_upstream then
-  		cookbook_file "#{confDir}/upstream.conf" do
-	  		source "upstream.conf"
-	  		cookbook cookbook_for_tpl
-			owner serviceUser
-			group serviceGroup
-			mode 0644
-  		end
-  	end
-
-  	# Nginx root configuration (http directives)
-  	node.set['nginx']['root_conf']['http']['includes'] = ['mime.types','vhosts/*.conf']
- 	nginx_root_conf service_id do
-		client_id client_id
-	end
-
-  	if is_atos then
-  		# with atos service tools, we need nginx.properties file
-  		template "#{confDir}/nginx.properties" do
-	  		source "nginx.properties.erb"
-	  		cookbook cookbook_for_tpl
-			owner serviceUser
-			group serviceGroup
-			mode 0644
-	  		variables({
-	    		'nginx_bin' => nginx_bin,
-	  		})
-	  	end
-
-	  	## Creating service tools dirs and conf files
-	  	directory "/etc/servicetools/services/#{client_id}" do
-			owner "root"
-			group "root"
-			mode 0755
-			action :create
-			recursive true
-		end
-		directory "/etc/servicetools/dispatchers/#{client_id}" do
-			owner "root"
-			group "root"
-			mode 0755
-			action :create
-			recursive true
-		end
-		template "/etc/servicetools/services/#{client_id}/#{service_id}" do 
-			source "servicetools-nginx-service-conf.erb"
-			cookbook cookbook_for_tpl
-			owner "root"
-			group "root"
-			mode 0644
-	  		variables({
-	    		'service_root' => serviceRootFolder,
-	  		})
-		end
-		execute "servicetools-dispatcher" do
-		  command "echo $(hostname) > /etc/servicetools/dispatchers/#{client_id}/front"
-		  action :run
-		end
-
-  	else
-  		template "/etc/init.d/#{new_resource.service_id}-nginx" do
-	  		source "init-script-nginx.erb"
-	  		cookbook cookbook_for_tpl
-			owner 'root'
-			group 'root'
-			mode 0755
-	  		variables({
-	    		'nginx_user' => serviceUser,
-	    		'nginx_group' => serviceGroup,
-	    		'service_root_folder' => serviceRootFolder,
-	    		'nginx_conf_dir' => confDir,
-	    		'nginx_tmp_dir' => tmpDir,
-	    		'nginx_bin' => nginx_bin,
-	    		'service_name' => service_id,
-	  		})
-	  	end
   	end
 end
--- dd-nginx/resources/service.rb
+++ dd-nginx/resources/service.rb
@@ -5,12 +5,9 @@ actions :create, :delete
 
 default_action :create
 
-attribute :prefix_folder, :kind_of => String, :default => "/WEBS"
-attribute :client_id, :kind_of => String, :required => true
+attribute :nginx_root_dir, :kind_of => String, :default => "/srv/webs"
 attribute :service_id, :kind_of => String, :name_attribute => true
 attribute :nginx_bin, :kind_of => String, :default => "/usr/sbin/nginx"
-attribute :with_upstream, :default => false
 attribute :service_user, :kind_of => String, :regex => Chef::Config[:user_valid_regex], :default => "www" 
 attribute :service_group, :kind_of => String, :regex => Chef::Config[:group_valid_regex], :default => "www"
-attribute :is_atos, :default => false
 attribute :cookbook, :kind_of => String, :default => 'nginx' # Use this to overwrite cookbook's templates provider
