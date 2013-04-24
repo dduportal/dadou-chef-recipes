cf30db8cb846443eac8e08aa1789ceaae99946c3
Damien DUPORTAL <damien.duportal@gmail.com>
2013-04-24 10:35:42 +0200

    deleting vagrant cookbook

--- vagrant/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'vagrant'
-maintainer       'Damien Duportal'
-maintainer_email 'damien.duportal@gmail.com'
-description      'Installs vagrant on your node'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
