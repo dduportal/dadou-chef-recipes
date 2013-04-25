898cc0b0e3789e4a213ce9a01b299dfb6963cff5
Damien DUPORTAL <damien.duportal@gmail.com>
2013-04-25 19:26:51 +0200

    Some tests on LWRPs of dd-nginx

--- dd-nginx/providers/service.rb
+++ dd-nginx/providers/service.rb
@@ -138,13 +138,13 @@ action :create do
   	service "#{new_resource.service_id} start" do
   		service_name "#{new_resource.service_id}"
   		action :start
-  		only_if "service #{new_resource.service_id}"
+  		not_if "service #{new_resource.service_id} status"
   	end
 
   	service "#{new_resource.service_id} reload" do
   		service_name "#{new_resource.service_id}"
   		action :reload
-  		not_if "service #{new_resource.service_id}"
+  		only_if "service #{new_resource.service_id} status"
   	end
 
 end
