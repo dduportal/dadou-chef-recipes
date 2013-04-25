87d2cfaafc42a96ae4585ead0d0539b3ed456d89
Damien DUPORTAL <damien.duportal@gmail.com>
2013-04-25 12:58:25 +0200

    Some tests on LWRPs of dd-nginx

--- dd-nginx/providers/service.rb
+++ dd-nginx/providers/service.rb
@@ -81,7 +81,7 @@ action :create do
 			'log_format' => ["main","'$remote_addr - $remote_user [$time_local] \"$request\" '","'$status $body_bytes_sent \"$http_referer\" '","'\"$http_user_agent\" \"$http_x_forwarded_for\"'"],
 			'access_log' => ["#{logs_dir}/access.log","main"],
 			'sendfile' => "on",
-			'keepalive_timeout' => "on",
+			'keepalive_timeout' => "65",
 			'include' => "#{conf_dir}/vhosts/*.conf",
 		}
 	}
