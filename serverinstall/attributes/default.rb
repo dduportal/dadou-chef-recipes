c5f62057671e26fd03acee6f88d13bd50245fe80
Damien DUPORTAL <damien.duportal@gmail.com>
2013-05-05 12:34:44 +0200

    Moving outufw and trying to get it functionnal

--- serverinstall/attributes/default.rb
+++ serverinstall/attributes/default.rb
@@ -16,7 +16,6 @@ node.set['vagrant']['url'] = "http://files.vagrantup.com/packages/7e400d00a3c5a0
 node.set['vagrant']['checksum'] = "061ea2fcc5a25c037acd07bf606b0ed0"
 node.set['vagrant']['plugins'] = ["vagrant-vbguest"]
 node.set['nginx']['version'] = "1.2.8"
-node.default['firewall']['state'] = "disable"
 node.default['firewall']['rules'] = {
 	'ssh' => {
 		'port' => '22',
