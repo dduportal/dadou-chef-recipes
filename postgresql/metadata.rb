362c8e9c1e327bc80437e97143f0af12aa1adc1a
Damien DUPORTAL <damien.duportal@gmail.com>
2013-05-06 08:23:10 +0200

    Adding missing metadata.rb

---
+++ postgresql/metadata.rb
@@ -0,0 +1,9 @@
+name             'postgresql'
+maintainer       'Damien Duportal'
+maintainer_email 'damien.duportal@gmail.com'
+description      'Installs postgresql on your node'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'yum'
+depends 'apt'
